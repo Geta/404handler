09d6d9297989b25e0bbc163833565a6f53dca891
Māris Krivtežs <marisks@gmail.com>
2017-12-12 10:30:25 +0200

    Removed version specific assemblyinfo.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -11,3 +11,5 @@ using System.Reflection;
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
