c3e2564ea94d1b10d5c1f9f53c24df25b4ccbff1
Māris Krivtežs <marisks@gmail.com>
2017-12-12 15:12:10 +0200

    Formatting and cleanup of data access.

--- src/Core/Data/DataStoreFactory.cs
+++ src/Core/Data/DataStoreFactory.cs
@@ -9,8 +9,8 @@ namespace BVNetwork.NotFound.Core.Data
             // GetStore will only return null the first time this method is called for a Type
             // In that case the ?? C# operator will call CreateStore
             // EPiServer.Data.Dynamic.DynamicDataStoreFactory.Instance.DeleteStore(t,true);
-            return EPiServer.Data.Dynamic.DynamicDataStoreFactory.Instance.GetStore(t) ??
-                EPiServer.Data.Dynamic.DynamicDataStoreFactory.Instance.CreateStore(t);
+            return EPiServer.Data.Dynamic.DynamicDataStoreFactory.Instance.GetStore(t)
+                ?? EPiServer.Data.Dynamic.DynamicDataStoreFactory.Instance.CreateStore(t);
         }
     }
 }
