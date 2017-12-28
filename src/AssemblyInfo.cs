2b05d898224237438dc547d24bf3342e5761f57e
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2017-12-28 11:21:20 +0200

    build updated to get versions from a single place (AssemblyInfo.CmsX.config), targets removed

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyTitle("BV Network 404 Handler")]
-[assembly: AssemblyDescription("Custom 404 Handler for EPiServer. See https://github.com/BVNetwork/404handler")]
+[assembly: AssemblyTitle("404 Handler for EPiServer")]
+[assembly: AssemblyDescription("This library contains a custom 404 handler for your EPiServer project. It will replace the default 404 handler with one that you can change, in order for it to have a more consistent look with the rest of your site. It will also handle more 404 cases than the built-in handler. It also includes a Custom Redirect Gadget (for EPiServer Online Center) for editors to add redirects. The 404 handler logs all 404 errors and present them in a list in the gadget, which will allow editors to easily add redirects based on the logged errors. See https://github.com/BVNetwork/404handler")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BV Network AS")]
 [assembly: AssemblyProduct("")]
