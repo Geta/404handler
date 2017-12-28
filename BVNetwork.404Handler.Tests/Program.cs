e8c79a6e05b2a0787a0d28fe573050d952fb23a0
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2017-12-28 18:08:42 +0200

    added tests for redirect specific url functionality

---
+++ BVNetwork.404Handler.Tests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSpec;
+using NSpec.Assertions;
+using NSpec.Domain;
+using NSpec.Domain.Formatters;
+
+namespace BVNetwork.NotFound
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                var tagOrClassName = "CustomRedirects";
+
+                var types = typeof(Program).GetTypeInfo().Assembly.GetTypes();
+                // OR
+                // var types = new Type[]{typeof(Some_Type_Containg_some_Specs)};
+
+                var finder = new SpecFinder(types, "");
+
+                var tagsFilter = new Tags().Parse(tagOrClassName);
+
+                var builder = new ContextBuilder(finder, tagsFilter, new DefaultConventions());
+
+                var runner = new ContextRunner(tagsFilter, new ConsoleFormatter(), false);
+
+                var results = runner.Run(builder.Contexts().Build());
+
+                //assert that there aren't any failures
+                (results.Failures().Count() == 0).ShouldBeTrue();
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ResetColor();
+                return 1;
+            }
+        }
+    }
+}
--- src/build-packages.cmd
+++ src/build-packages.cmd
@@ -1,3 +1,25 @@
 @echo off
-del *.nupkg
-msbuild BVNetwork.404Handler.csproj /t:Rebuild /p:Configuration=Release /v:m
+del *.nupkg 2> nul
+
+echo building CMS11
+msbuild BVNetwork.404Handler.sln /t:Rebuild /p:Configuration=Release /v:m
+rem echo %ErrorLevel%
+if "%ErrorLevel%" neq "0" goto error
+echo CMS11 built
+
+call ..\BVNetwork.404Handler.Tests\bin\Release\BVNetwork.404Handler.Tests.exe
+rem echo %ErrorLevel%
+if "%ErrorLevel%" neq "0" goto error
+
+
+:end        
+	echo Build successfully completed
+	dir /b *.nupkg
+	goto fin
+
+:error      
+	del *.nupkg 2>nul
+	echo "Build FAILED, see output for details. Build results (if any) have been removed. "
+	goto fin
+
+:fin
