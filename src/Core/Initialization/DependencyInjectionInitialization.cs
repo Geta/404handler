7c07b18c4b9b75ba0b84fe6c1bea5fac5ee449c6
Māris Krivtežs <marisks@gmail.com>
2018-02-08 12:00:36 +0200

    Added dependency injection initialization.

---
+++ src/Core/Initialization/DependencyInjectionInitialization.cs
@@ -0,0 +1,28 @@
+﻿using BVNetwork.NotFound.Core.Configuration;
+using BVNetwork.NotFound.Core.CustomRedirects;
+using BVNetwork.NotFound.Core.Logging;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace BVNetwork.NotFound.Core.Initialization
+{
+    [ModuleDependency(typeof(ServiceContainerInitialization))]
+    public class DependencyInjectionInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Services.AddSingleton<IConfiguration>(Configuration.Configuration.Instance);
+            context.Services.AddSingleton<IRequestLogger>(RequestLogger.Instance);
+            context.Services.AddTransient<IRedirectHandler>(_ => CustomRedirectHandler.Current); // Load per-request as it is read from the cache
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
