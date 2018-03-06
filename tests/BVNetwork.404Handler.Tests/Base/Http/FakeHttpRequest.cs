0cbc5995b67482eb25403ec08232374a6bc6bb64
Māris Krivtežs <marisks@gmail.com>
2018-03-06 12:39:29 +0200

    Covered with tests NotFoundPageUtil.GetUrlNotFound.

--- tests/BVNetwork.404Handler.Tests/Base/Http/FakeHttpRequest.cs
+++ tests/BVNetwork.404Handler.Tests/Base/Http/FakeHttpRequest.cs
@@ -6,8 +6,23 @@ namespace BVNetwork.NotFound.Tests.Base.Http
 {
     public class FakeHttpRequest : HttpRequestBase
     {
-        public override Uri Url { get; } = new Uri("http://example.com");
+        private const string QueryStringVariable = "QUERY_STRING";
+
+        private Uri _uri = new Uri("http://example.com");
+        public override Uri Url => _uri;
         public override NameValueCollection ServerVariables { get; } = new NameValueCollection();
         public override Uri UrlReferrer { get; } = new Uri("http://example.com/home");
+
+        public FakeHttpRequest WithQueryString(string value)
+        {
+            ServerVariables.Add(QueryStringVariable, value);
+            return this;
+        }
+
+        public FakeHttpRequest WithUrl(string expected)
+        {
+            _uri = string.IsNullOrEmpty(expected) ? null : new Uri(expected);
+            return this;
+        }
     }
 }
---
+++ tests/BVNetwork.404Handler.Tests/NotFoundPage/NotFoundPageUtilTests.cs
@@ -0,0 +1,52 @@
+﻿using BVNetwork.NotFound.Core.NotFoundPage;
+using BVNetwork.NotFound.Tests.Base.Http;
+using Xunit;
+
+namespace BVNetwork.NotFound.Tests.NotFoundPage
+{
+    public class NotFoundPageUtilTests
+    {
+
+        [Fact]
+        public void GetUrlNotFound_returns_null_when_no_query_string_provided()
+        {
+            var request = new FakeHttpRequest();
+
+            var actual = NotFoundPageUtil.GetUrlNotFound(request);
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void GetUrlNotFound_returns_null_when_query_string_is_null()
+        {
+            var request = new FakeHttpRequest().WithQueryString(null);
+
+            var actual = NotFoundPageUtil.GetUrlNotFound(request);
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void GetUrlNotFound_returns_null_when_query_starts_with_aspxerrorpath_and_url_is_null()
+        {
+            var request = new FakeHttpRequest().WithUrl(null).WithQueryString("aspxerrorpath=/missing");
+
+            var actual = NotFoundPageUtil.GetUrlNotFound(request);
+
+            Assert.Null(actual);
+        }
+
+        [Theory]
+        [InlineData("404;http://mytest.localtest.me/missing", "http://mytest.localtest.me/missing")]
+        [InlineData("aspxerrorpath=/missing", "http://mytest.localtest.me/missing")]
+        public void GetUrlNotFound_returns_url(string queryString, string expected)
+        {
+            var request = new FakeHttpRequest().WithUrl(expected).WithQueryString(queryString);
+
+            var actual = NotFoundPageUtil.GetUrlNotFound(request);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
