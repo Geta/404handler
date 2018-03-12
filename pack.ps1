1595852c511860a74413b1fa8d6775a101d8df90
Māris Krivtežs <marisks@gmail.com>
2018-03-12 19:13:29 +0200

    Updated package scripts.

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,5 @@
 $outputDir = ".\package\"
 $build = "Release"
-$version = "11.0.0"
+$version = "11.1.4"
 
 nuget.exe pack ".\src\BVNetwork.404Handler.csproj" -IncludeReferencedProjects -properties Configuration=$build -Version $version -OutputDirectory $outputDir
--- pack_cms10.ps1
+++ pack_cms10.ps1
@@ -1,5 +1,5 @@
 $outputDir = ".\package\"
 $build = "Release"
-$version = "10.2.0"
+$version = "10.3.3"
 
 nuget.exe pack ".\src\BVNetwork.404Handler.Cms10.csproj" -IncludeReferencedProjects -properties Configuration=$build -Version $version -OutputDirectory $outputDir
