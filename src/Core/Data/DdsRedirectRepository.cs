cc8f3aa4973c600acd0a9c18763eec7f491f30e7
Māris Krivtežs <marisks@gmail.com>
2018-12-11 01:09:14 +0200

    Cleanup.

--- src/Core/Data/DdsRedirectRepository.cs
+++ src/Core/Data/DdsRedirectRepository.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using BVNetwork.NotFound.Core.CustomRedirects;
-using EPiServer.Data;
 using EPiServer.Data.Dynamic;
 
 namespace BVNetwork.NotFound.Core.Data
