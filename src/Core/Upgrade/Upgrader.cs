88622b43aa716b6846e18d7da863184b513952d8
Māris Krivtežs <marisks@gmail.com>
2018-12-11 23:04:07 +0200

    Refactoring upgrader table creation.

--- src/Core/Upgrade/Upgrader.cs
+++ src/Core/Upgrade/Upgrader.cs
@@ -22,54 +22,74 @@ namespace BVNetwork.NotFound.Core.Upgrade
             }
         }
         /// <summary>
-        /// Create redirects table and SP for version number
+        /// Create redirects and suggestions tables and SP for version number
         /// </summary>
         private static void Create()
         {
             var dba = DataAccessBaseEx.GetWorker();
 
-            Log.Information("Create 404 handler redirects table START");
+            var created = CreateSuggestionsTable(dba);
+
+            if (created)
+            {
+                created = CreateVersionNumberSp(dba);
+            }
+
+            Valid = created;
+        }
+
+        private static bool CreateSuggestionsTable(DataAccessBaseEx dba)
+        {
+            Log.Information("Create 404 handler suggestions table START");
             var createTableScript = @"CREATE TABLE [dbo].[BVN.NotFoundRequests](
-	                                    [ID] [int] IDENTITY(1,1) NOT NULL,
-	                                    [OldUrl] [nvarchar](2000) NOT NULL,
-	                                    [Requested] [datetime] NULL,
-	                                    [Referer] [nvarchar](2000) NULL
+                                        [ID] [int] IDENTITY(1,1) NOT NULL,
+                                        [OldUrl] [nvarchar](2000) NOT NULL,
+                                        [Requested] [datetime] NULL,
+                                        [Referer] [nvarchar](2000) NULL
                                         ) ON [PRIMARY]";
-            var create = dba.ExecuteNonQuery(createTableScript);
+            var created = dba.ExecuteNonQuery(createTableScript);
+            Log.Information("Create 404 handler suggestions table END");
 
-            Log.Information("Create 404 handler redirects table END");
-
-
-            if (create)
+            if (created)
             {
-                Log.Information("Create 404 handler version SP START");
-                var versionSp = @"CREATE PROCEDURE [dbo].[bvn_notfoundversion] AS RETURN " + Configuration.Configuration.CurrentVersion;
-
-                if (!dba.ExecuteNonQuery(versionSp))
-                {
-                    create = false;
-                    Log.Error("An error occured during the creation of the 404 handler version stored procedure. Canceling.");
-                }
-
-                Log.Information("Create 404 handler version SP END");
+                created = CreateSuggestionsTableIndex(dba);
             }
 
-            if (create)
+            return created;
+        }
+
+        private static bool CreateSuggestionsTableIndex(DataAccessBaseEx dba)
+        {
+            Log.Information("Create suggestions table clustered index START");
+            var clusteredIndex =
+                "CREATE CLUSTERED INDEX NotFoundRequests_ID ON [dbo].[BVN.NotFoundRequests] (ID)";
+
+            var created = dba.ExecuteNonQuery(clusteredIndex);
+            if (!created)
             {
-                Log.Information("Create Clustered index START");
-                var clusteredIndex =
-                    "CREATE CLUSTERED INDEX NotFoundRequests_ID ON [dbo].[BVN.NotFoundRequests] (ID)";
-
-                if (!dba.ExecuteNonQuery(clusteredIndex))
-                {
-                    create = false;
-                    Log.Error("An error occurred during the creation of the 404 handler redirects clustered index. Canceling.");
-                }
-
-                Log.Information("Create Clustered index END");
+                created = false;
+                Log.Error("An error occurred during the creation of the 404 handler redirects clustered index. Canceling.");
             }
 
-            Valid = create;
+            Log.Information("Create suggestions table clustered index END");
+            return created;
+        }
+
+        private static bool CreateVersionNumberSp(DataAccessBaseEx dba)
+        {
+            Log.Information("Create 404 handler version SP START");
+            var versionSp =
+                $@"CREATE PROCEDURE [dbo].[bvn_notfoundversion] AS RETURN {Configuration.Configuration.CurrentVersion}";
+
+            var created = dba.ExecuteNonQuery(versionSp);
+
+            if (!created)
+            {
+                Log.Error("An error occured during the creation of the 404 handler version stored procedure. Canceling.");
+            }
+
+            Log.Information("Create 404 handler version SP END");
+            return created;
         }
 
         private static void Upgrade()
