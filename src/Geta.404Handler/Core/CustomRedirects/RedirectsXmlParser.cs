8f38de9072ca1aad891f998b2d0677423ff26c36
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-01-20 11:01:01 +0200

    * restrucxtured folders * renamed projects to Geta.404Handler * dropped CMS10 support

---
+++ src/Geta.404Handler/Core/CustomRedirects/RedirectsXmlParser.cs
@@ -0,0 +1,128 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using EPiServer.Logging;
+
+namespace BVNetwork.NotFound.Core.CustomRedirects
+{
+    /// <summary>
+    /// Class for reading and writing to the custom redirects file
+    /// </summary>
+    public class RedirectsXmlParser
+    {
+        private static readonly ILogger Logger = LogManager.GetLogger();
+        private readonly XmlDocument _customRedirectsXmlFile;
+
+        private const string Newurl = "new";
+        private const string Oldurl = "old";
+        private const string Skipwildcard = "onWildCardMatchSkipAppend";
+
+        /// <summary>
+        /// Reads the custom redirects information from the specified xml file
+        /// </summary>
+        public RedirectsXmlParser(Stream xmlContent)
+        {
+            _customRedirectsXmlFile = new XmlDocument();
+            if (xmlContent != null)
+            {
+                _customRedirectsXmlFile.Load(xmlContent);
+            }
+            else
+            {
+                // Not on disk, not in a vpp, construct an empty one
+                _customRedirectsXmlFile = new XmlDocument
+                {
+                    InnerXml = "<redirects><urls></urls></redirects>"
+                };
+                Logger.Error("404 Handler: The Custom Redirects file does not exist.");
+            }
+        }
+
+        public RedirectsXmlParser()
+        {
+        }
+
+        /// <summary>
+        /// Parses the xml file and reads all redirects.
+        /// </summary>
+        /// <returns>A collection of CustomRedirect objects</returns>
+        public CustomRedirectCollection Load()
+        {
+            const string urlpath = "/redirects/urls/url";
+
+            var redirects = new CustomRedirectCollection();
+
+            // Parse all url nodes
+            var nodes = _customRedirectsXmlFile.SelectNodes(urlpath);
+            foreach (XmlNode node in nodes)
+            {
+                // Each url new url can have several old values
+                // we need to create a redirect object for each pair
+                var newNode = node.SelectSingleNode(Newurl);
+
+                var oldNodes = node.SelectNodes(Oldurl);
+                foreach (XmlNode oldNode in oldNodes)
+                {
+                    var skipWildCardAppend = false;
+                    var skipWildCardAttr = oldNode.Attributes[Skipwildcard];
+                    if (skipWildCardAttr != null)
+                    {
+                        // If value parsing fails, it will be false by default. We do
+                        // not really care to check if it fails, as we cannot do anything
+                        // about it (throwing an exception is not a good idea here)
+                        bool.TryParse(skipWildCardAttr.Value, out skipWildCardAppend);
+                    }
+
+                    // Create new custom redirect nodes
+                    var redirect = new CustomRedirect(oldNode.InnerText, newNode.InnerText, skipWildCardAppend);
+                    redirects.Add(redirect);
+                }
+            }
+
+            return redirects;
+        }
+
+        public XmlDocument Export(List<CustomRedirect> redirects)
+        {
+            var document = new XmlDocument();
+            var xmlDeclaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);
+            var root = document.DocumentElement;
+            document.InsertBefore(xmlDeclaration, root);
+
+            var redirectsElement = document.CreateElement(string.Empty, "redirects", string.Empty);
+            document.AppendChild(redirectsElement);
+
+            var urlsElement = document.CreateElement(string.Empty, "urls", string.Empty);
+            redirectsElement.AppendChild(urlsElement);
+
+            foreach (var redirect in redirects)
+            {
+                if (string.IsNullOrWhiteSpace(redirect.OldUrl) || string.IsNullOrWhiteSpace(redirect.NewUrl))
+                {
+                    continue;
+                }
+
+                var urlElement = document.CreateElement(string.Empty, "url", string.Empty);
+
+                var oldElement = document.CreateElement(string.Empty, Oldurl, string.Empty);
+                oldElement.AppendChild(document.CreateTextNode(redirect.OldUrl.Trim()));
+                if (redirect.WildCardSkipAppend)
+                {
+                    var wildCardAttribute = document.CreateAttribute(string.Empty, Skipwildcard, string.Empty);
+                    wildCardAttribute.Value = "true";
+                    oldElement.Attributes.Append(wildCardAttribute);
+                }
+
+                var newElement = document.CreateElement(string.Empty, Newurl, string.Empty);
+                newElement.AppendChild(document.CreateTextNode(redirect.NewUrl.Trim()));
+
+                urlElement.AppendChild(oldElement);
+                urlElement.AppendChild(newElement);
+
+                urlsElement.AppendChild(urlElement);
+            }
+
+            return document;
+        }
+    }
+}
---
+++ src/Geta.404Handler/Core/Upgrade/Upgrader.cs
@@ -0,0 +1,164 @@
+﻿using BVNetwork.NotFound.Core.Data;
+
+using EPiServer.Logging;
+
+namespace BVNetwork.NotFound.Core.Upgrade
+{
+    public static class Upgrader
+    {
+        private static readonly ILogger Logger = LogManager.GetLogger();
+
+        public static bool Valid { get; set; }
+
+        public static void Start(int version)
+        {
+            if (version == -1)
+            {
+                Create();
+            }
+            else
+            {
+                Upgrade();
+            }
+        }
+        /// <summary>
+        /// Create redirects and suggestions tables and SP for version number
+        /// </summary>
+        private static void Create()
+        {
+            var dba = DataAccessBaseEx.GetWorker();
+
+            var created = CreateRedirectsTable(dba);
+
+            if (created)
+            {
+                created = CreateSuggestionsTable(dba);
+            }
+
+            if (created)
+            {
+                created = CreateVersionNumberSp(dba);
+            }
+
+            Valid = created;
+        }
+
+        private static bool CreateRedirectsTable(DataAccessBaseEx dba)
+        {
+            Logger.Information("Create 404 handler redirects table START");
+            var createTableScript = @"CREATE TABLE [dbo].[404Handler.Redirects](
+                                        [Id] [uniqueidentifier] NOT NULL,
+                                        [OldUrl] [nvarchar](2000) NOT NULL,
+                                        [NewUrl] [nvarchar](2000) NOT NULL,
+                                        [State] [int] NOT NULL,
+                                        [WildCardSkipAppend] [bit] NOT NULL,
+                                        CONSTRAINT [PK_404HandlerRedirects] PRIMARY KEY CLUSTERED ([Id] ASC) ON [PRIMARY]
+                                        ) ON [PRIMARY]";
+            var created = dba.ExecuteNonQuery(createTableScript);
+            Logger.Information("Create 404 handler redirects table END");
+
+            return created;
+        }
+
+        private static bool CreateSuggestionsTable(DataAccessBaseEx dba)
+        {
+            Logger.Information("Create 404 handler suggestions table START");
+            var createTableScript = @"CREATE TABLE [dbo].[BVN.NotFoundRequests](
+                                        [ID] [int] IDENTITY(1,1) NOT NULL,
+                                        [OldUrl] [nvarchar](2000) NOT NULL,
+                                        [Requested] [datetime] NULL,
+                                        [Referer] [nvarchar](2000) NULL
+                                        ) ON [PRIMARY]";
+            var created = dba.ExecuteNonQuery(createTableScript);
+            Logger.Information("Create 404 handler suggestions table END");
+
+            if (created)
+            {
+                created = CreateSuggestionsTableIndex(dba);
+            }
+
+            return created;
+        }
+
+        private static bool CreateSuggestionsTableIndex(DataAccessBaseEx dba)
+        {
+            Logger.Information("Create suggestions table clustered index START");
+            var clusteredIndex =
+                "CREATE CLUSTERED INDEX NotFoundRequests_ID ON [dbo].[BVN.NotFoundRequests] (ID)";
+
+            var created = dba.ExecuteNonQuery(clusteredIndex);
+            if (!created)
+            {
+                Logger.Error("An error occurred during the creation of the 404 handler redirects clustered index. Canceling.");
+            }
+
+            Logger.Information("Create suggestions table clustered index END");
+            return created;
+        }
+
+        private static bool CreateVersionNumberSp(DataAccessBaseEx dba)
+        {
+            Logger.Information("Create 404 handler version SP START");
+            var versionSp =
+                $@"CREATE PROCEDURE [dbo].[bvn_notfoundversion] AS RETURN {Configuration.Configuration.CurrentVersion}";
+
+            var created = dba.ExecuteNonQuery(versionSp);
+
+            if (!created)
+            {
+                Logger.Error("An error occurred during the creation of the 404 handler version stored procedure. Canceling.");
+            }
+
+            Logger.Information("Create 404 handler version SP END");
+            return created;
+        }
+
+        private static void Upgrade()
+        {
+            var dba = DataAccessBaseEx.GetWorker();
+
+            if (!TableExists("404Handler.Redirects", dba))
+            {
+                Valid = CreateRedirectsTable(dba);
+            }
+
+            if (!SuggestionsTableIndexExists(dba))
+            {
+                Valid = CreateSuggestionsTableIndex(dba);
+            }
+
+            if (Valid)
+            {
+                UpdateVersionNumber(dba);
+            }
+        }
+
+        private static bool TableExists(string tableName, DataAccessBaseEx dba)
+        {
+            var cmd = $@"SELECT *
+                 FROM INFORMATION_SCHEMA.TABLES
+                 WHERE TABLE_SCHEMA = 'dbo'
+                 AND  TABLE_NAME = '{tableName}'";
+            var num = dba.ExecuteScalar(cmd);
+            return num != 0;
+        }
+
+        private static bool SuggestionsTableIndexExists(DataAccessBaseEx dba)
+        {
+            var indexCheck =
+                $@"SELECT COUNT(*)
+                 FROM sys.indexes
+                 WHERE name='NotFoundRequests_ID' AND object_id = OBJECT_ID('[dbo].[BVN.NotFoundRequests]')";
+
+            var num = dba.ExecuteScalar(indexCheck);
+            return num != 0;
+        }
+
+        private static void UpdateVersionNumber(DataAccessBaseEx dba)
+        {
+            var versionSp =
+                $@"ALTER PROCEDURE [dbo].[bvn_notfoundversion] AS RETURN {Configuration.Configuration.CurrentVersion}";
+            Valid = dba.ExecuteNonQuery(versionSp);
+        }
+    }
+}
