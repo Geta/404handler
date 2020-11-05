0e6dde8aaf0e6a1f80f2f722794805795288ddab
Cynthia Richard <crichard@absolunet.com>
2020-11-05 09:37:08 -0500

    Fix encoding

--- src/Geta.404Handler/Core/Upgrade/Upgrader.cs
+++ src/Geta.404Handler/Core/Upgrade/Upgrader.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Geta Digital. All rights reserved.
+﻿// Copyright (c) Geta Digital. All rights reserved.
 // Licensed under Apache-2.0. See the LICENSE file in the project root for more information
 
 using BVNetwork.NotFound.Core.Data;
